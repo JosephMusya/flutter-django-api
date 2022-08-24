4d4101f0bc0afe39f7c8e4f3856e685bc7ed09c8
JosephMusya <josephmusya254@gmail.com>
2022-08-24 21:41:22 +0300

    Added POST functionality into the API

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from .serializers import BookSerializer
 from rest_framework import generics
 from .models import Book
-class BookList(generics.ListAPIView):
+class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer  
